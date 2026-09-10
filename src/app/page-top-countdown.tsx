f4d50367132e73c1fb0c3e74ba07c03c854c87a8
miki <miki.a.takakura@ai-communication.jp>
2026-09-10 17:37:38 +0900

    fix:sp

--- src/app/page-top-countdown.tsx
+++ src/app/page-top-countdown.tsx
@@ -4,7 +4,7 @@ import PageTop from "./page-top";
 import CountdownSection from "./PageSections/countdownSection";
 
 export default function PageTopCountdown() {
-    const targetDate = new Date('2026-09-30T00:00:00+09:00');
+    const targetDate = new Date('2026-08-30T00:00:00+09:00');
     const [isExpired, setIsExpired] = useState(targetDate.getTime() < new Date().getTime());
 
     return (
